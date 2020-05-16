351318fd032e3adc62a6dc7dacfb3e16b35484da
Stunbox <54955423+Stunbox@users.noreply.github.com>
2020-05-16 13:07:23 -0500

    Decrease Stock
    :)

--- tiendita.sql
+++ tiendita.sql
@@ -5,3 +5,12 @@ ON venta
 FOR EACH ROW 
 UPDATE producto SET stock= stock+ deleted.Cantidad
 WHERE idProducto= (SELECT idProducto FROM venta_producto WHERE idVenta= deleted.idVenta);
+
+-- Para decrementar el stock cuando se vende un producto
+DELIMITER \\
+Create Trigger DecreaseStock after insert on venta_producto
+For each row 
+BEGIN 
+      update producto set Stock = (Stock - new.Cantidad) where idProducto = new.idProducto;
+END \\
+DELIMITER ;
