3e750a4e5728e067538bd30f43e3ebe0580fbc32
jar8-stack <bioshock450226@hotmsil.com>
2020-05-19 20:29:09 -0500

    Segundo disparador said

--- tiendita.sql
+++ tiendita.sql
@@ -5,3 +5,20 @@ ON venta
 FOR EACH ROW 
 UPDATE producto SET stock= stock+ deleted.Cantidad
 WHERE idProducto= (SELECT idProducto FROM venta_producto WHERE idVenta= deleted.idVenta);
+
+-- - Guardar en otra tabla los cambios de precio que tenga un producto. Cambios_precio (id, idProducto, precio_anterior, precio_nuevo).
+SELECT * FROM producto;
+UPDATE producto set precio=15 WHERE idProducto=2;
+
+DROP TRIGGER cambio_Precio;
+
+SELECT * FROM info_Cambio;
+
+CREATE TABLE info_Cambio(
+id_Product INT PRIMARY KEY,
+precioCambio FLOAT
+);
+CREATE TRIGGER cambio_Precio AFTER UPDATE 
+ON producto 
+FOR EACH ROW 
+INSERT INTO info_Cambio VALUES(OLD.idProducto, NEW.precio);
